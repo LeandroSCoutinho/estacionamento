934fae3906b4f76ebc7cdfdb3e04a4318269779b
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2022-07-10 20:30:31 -0300

    Add end render

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 "use strict";
 (function () {
     var _a;
-    const seletor = (query) => document.querySelector(query);
-    (_a = seletor("#cadastrar")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
+    const $ = (query) => document.querySelector(query);
+    function patio() {
+        function ler() { }
+        function adicionar(veiculo) {
+            var _a;
+            const row = document.createElement("tr");
+            row.innerHTML = `
+                <th>${veiculo.nome}</th>
+                <th>${veiculo.placa}</th>
+                <th>${veiculo.entrada}</th>
+                <th>
+                    <button type="button" class="delete" data-placa="${veiculo.placa}" >x</button>
+                </th>
+            `;
+            (_a = $("#patio")) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+        }
+        function salvar() { }
+        function remover() { }
+        function renderizar() { }
+        return { ler, adicionar, salvar, remover, renderizar };
+    }
+    (_a = $("#cadastrar")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
         var _a, _b;
-        const nome = (_a = seletor("#nome")) === null || _a === void 0 ? void 0 : _a.value;
-        const placa = (_b = seletor("#placa")) === null || _b === void 0 ? void 0 : _b.value;
+        const nome = (_a = $("#nome")) === null || _a === void 0 ? void 0 : _a.value;
+        const placa = (_b = $("#placa")) === null || _b === void 0 ? void 0 : _b.value;
+        console.log(nome, placa);
         if (!nome || !placa) {
             alert("Os campos nome e placa do veículo são obrigatórios");
             return;
         }
+        patio().adicionar({ nome, placa, entrada: new Date });
     });
 })();
--- script.ts
+++ script.ts
@@ -1,13 +1,62 @@
+interface Veiculo {
+    nome: string;
+    placa: string;
+    entrada: Date;
+}
+
 (function(){
     const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+    function patio(){
+        function ler(): Veiculo[]{
+            return localStorage.patio ? JSON.parse(localStorage.patio): [];
+        }
+
+        function salvar(veiculos: Veiculo[]){
+            localStorage.setItem("patio", JSON.stringify(veiculos));
+        }
+
+        function adicionar(veiculo: Veiculo, salva?: boolean){
+            const row =  document.createElement("tr");
+
+            row.innerHTML = `
+                <th>${veiculo.nome}</th>
+                <th>${veiculo.placa}</th>
+                <th>${veiculo.entrada}</th>
+                <th>
+                    <button class="delete" data-placa=${veiculo.placa} >x</button>
+                </th>
+            `;
+
+            $("#patio")?.appendChild(row);
+
+           if(salva) salvar([...ler(), veiculo])
+        }
+
+        function renderizar(){
+            $("#patio")!.innerHTML = "";
+            const patio = ler();
+
+            if(patio.length){
+                patio.forEach((veiculo) => adicionar(veiculo))
+            }
+        }
+
+        function remover(){}
+
+        return {ler, adicionar, salvar, remover, renderizar}
+    }
+
+    patio().renderizar();
 
     $("#cadastrar")?.addEventListener("click",() => {
         const nome = $("#nome")?.value;
         const placa = $("#placa")?.value;
-
+        console.log(nome, placa);
         if(!nome || !placa){
             alert("Os campos nome e placa do veículo são obrigatórios");
             return;
         }
+        patio().adicionar({nome, placa, entrada: new Date}, true);
     })
 })();
